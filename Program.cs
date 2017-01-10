a32e220de6697a984ef44f0e9cc74101deee3918
Donovan An <donovantaean@yahoo.com>
2017-01-10 13:46:44 -0800

    completed dictionary challenges

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ConsoleApplication
 {
@@ -35,7 +36,46 @@ namespace ConsoleApplication
             Console.WriteLine(array2D[7,7]);
 
 
+            // dictionary challenges
+            Dictionary<string,string> tim = new Dictionary<string,string>();
             
+            tim.Add("name", "Tim");
+            tim.Add("favSports", "Football");
+            tim.Add("numPets", "1");
+            tim.Add("likesIceCream", "true");
+
+            Dictionary<string,string> martin = new Dictionary<string,string>();
+            
+            martin.Add("name", "Martin");
+            martin.Add("favSports", "Baseball");
+            martin.Add("numPets", "3");
+            martin.Add("likesIceCream", "true");
+
+            Dictionary<string,string> nikki = new Dictionary<string,string>();
+            
+            nikki.Add("name", "Nikki");
+            nikki.Add("favSports", "Basketball");
+            nikki.Add("numPets", "1");
+            nikki.Add("likesIceCream", "true");
+
+            Dictionary<string,string> sara = new Dictionary<string,string>();
+            
+            sara.Add("name", "Sara");
+            sara.Add("favSports", "Football");
+            sara.Add("numPets", "0");
+            sara.Add("likesIceCream", "true");
+
+            List<Dictionary<string,string>> myFriends = new List<Dictionary<string,string>>();
+            myFriends.Add(tim);
+            myFriends.Add(martin);
+            myFriends.Add(nikki);
+            myFriends.Add(sara);
+
+            foreach( var friend in myFriends){
+                foreach( var info in friend){
+                    Console.WriteLine(info.Key + " - " + info.Value);
+                }
+            }
         }
     }
 }
