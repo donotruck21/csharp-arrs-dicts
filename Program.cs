275b2a2ab17f024371025db32a3cdc4c65d9d9e3
Donovan An <donovantaean@yahoo.com>
2017-01-10 12:54:41 -0800

    finished array challenges

--- Program.cs
+++ Program.cs
@@ -7,6 +7,20 @@ namespace ConsoleApplication
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            int[] numArray = new int[10];
+            string[] nameArray = {"Tim", "Martin", "Nikki", "Sara"};
+            bool[] boolArray = new bool[10];
+
+            for(int i = 0; i < boolArray.Length; i++){
+                if(i % 2 == 0){
+                    boolArray[i] = true;
+                } else {
+                    boolArray[i] = false;
+                }
+            }
+
+            // Console.WriteLine(boolArray);
+            Console.WriteLine("[{0}]", string.Join(", ", boolArray));
         }
     }
 }
